6beff6c6e278d97d42ce344624e2cd8efafd28b4
kaleenchen <kaleenchen@berkeley.edu>
2024-11-21 18:56:33 -0800

    social thread tab

---
+++ app/(tabs)/social_thread.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import SocialPost from '@/components/SocialPost'
+
+const social_thread = () => {
+  return (
+    <View>
+      <Text>Spoony</Text>
+      <SocialPost></SocialPost>
+    </View>
+  )
+}
+
+export default social_thread
